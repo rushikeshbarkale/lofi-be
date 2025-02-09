1b3e5cecd819f76ebc5c92b52bf78b5766eea1d2
Rushikesh Barkale <rushikesh@turnkey.work>
2025-02-10 02:03:30 +0530

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
-const allowedOrigins = ["https://fantasy-beats.netlify.app/"];
+const allowedOrigins = ["https://fantasy-beats.netlify.app"];
 
 app.use(
   cors({
